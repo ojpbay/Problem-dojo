80be66fd18c4b20c461c33dd21f7714e8ce3bed3
ojpbay <oli.baylis@live.co.uk>
2022-12-10 15:40:58 +0000

    Working example

--- ProblemDojo/ReverseInteger.cs
+++ ProblemDojo/ReverseInteger.cs
@@ -1,11 +1,25 @@
-﻿namespace ProblemDojo
+﻿using System;
+
+namespace ProblemDojo
 {
     public static class ReverseInteger
     {
-        public static int Reverse(int source)
+        public static int Reverse(int num)
         {
-            // TODO
-            throw new NotImplementedException();
+            var isNegative = num < 0;
+
+            if (isNegative)
+            {
+                num *= -1;
+            }
+
+            int result = 0;
+            while (num > 0)
+            {
+                result = result * 10 + num % 10;
+                num /= 10;
+            }
+            return isNegative ? result * -1 : result;
         }
     }
 }
