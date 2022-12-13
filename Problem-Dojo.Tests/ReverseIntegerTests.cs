6f84d542cb53e5003b38b6b9f8eee0b68281417c
ojpbay <oli.baylis@live.co.uk>
2022-12-13 17:28:28 +0000

    Rename tests

--- Problem-Dojo.Tests/ReverseIntegerTests.cs
+++ Problem-Dojo.Tests/ReverseIntegerTests.cs
@@ -1,47 +1,30 @@
 namespace Problem_Dojo.Tests
 {
     public class ReverseIntegerTests
-    {
+    {        
+        [Fact]
+        public void GivenSourceShouldReturnSourceReversed()
+        {
+            var result = ReverseInteger.Reverse(123456);
+
+
+            Assert.Equal(654321, result);
+        }
+
+        [Fact]
+        public void GivenSourceWithTrailingZerosShouldReturnResultWithNoLeadingZeros()
+        {
+            var result = ReverseInteger.Reverse(1200);
+
+            Assert.Equal(21, result);
+        }
+
         [Fact]
         public void GivenNegativeSourceShouldReturnNegativeResult()
         {
             var result = ReverseInteger.Reverse(-123);
 
             Assert.Equal(-321, result);
-        }
-
-        [Fact]
-        public void Given123ShouldReturn321()
-        {
-            var result = ReverseInteger.Reverse(123);
-
-
-            Assert.Equal(321, result);
-        }
-
-        [Fact]
-        public void Given214748364ShouldReturn463847412()
-        {
-            var result = ReverseInteger.Reverse(214748364);
-
-
-            Assert.Equal(463847412, result);
-        }
-
-        [Fact]
-        public void GivenTrailingZeroInSourceShouldReturnResultWithoutLeadingZero()
-        {
-            var result = ReverseInteger.Reverse(120);
-
-            Assert.Equal(21, result);
-        }
-
-        [Fact]
-        public void GivenTrailingZerosInSourceShouldReturnResultWithoutLeadingZeros()
-        {
-            var result = ReverseInteger.Reverse(3400);
-
-            Assert.Equal(43, result);
         }
     }
 }
