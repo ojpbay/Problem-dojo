4641a6d3fe6d8be49769fbe7e2bdcaee810656d7
Oliver Baylis <oliver.baylis@willistowerswatson.com>
2022-12-14 11:09:49 +0000

    IV 14-12-2022

--- Problem-Dojo/ReverseInteger.cs
+++ Problem-Dojo/ReverseInteger.cs
@@ -1,10 +1,23 @@
-﻿namespace Problem_Dojo
+﻿using System.Text;
+
+namespace Problem_Dojo
 {
     public static class ReverseInteger
     {
         public static int Reverse(int num)
         {
-            throw new NotImplementedException();
+            var reversedNumValue = new StringBuilder();
+
+            var numChars = num.ToString().ToCharArray().Select(c => c.ToString()).Where(c => c != "-").ToArray();
+
+            for (var i = numChars.Length - 1; i > -1; i--)
+            {
+                reversedNumValue.Append(numChars[i]);
+            }
+
+            var reversedNum = int.Parse(reversedNumValue.ToString());
+
+            return (num >= 0) ? reversedNum : (-1) * reversedNum;
         }
     }
 }
