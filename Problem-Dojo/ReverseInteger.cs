97b1f08256f4f604002b12638314c063c887d001
Oliver Baylis <oliver.baylis@willistowerswatson.com>
2022-12-14 10:24:00 +0000

    Remove solution

--- Problem-Dojo/ReverseInteger.cs
+++ Problem-Dojo/ReverseInteger.cs
@@ -1,30 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Problem_Dojo
+﻿namespace Problem_Dojo
 {
     public static class ReverseInteger
     {
         public static int Reverse(int num)
         {
-            var isNegative = num < 0;
-
-            if (isNegative)
-            {
-                num *= -1;
-            }
-
-            int result = 0;
-            while (num > 0)
-            {
-                result = result * 10 + num % 10;
-                num /= 10;
-            }
-
-            return isNegative ? result * -1 : result;
+            throw new NotImplementedException();
         }
     }
 }
